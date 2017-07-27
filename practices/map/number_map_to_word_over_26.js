5ecc548a8cc9f527755a7d895eee7ed4126df86f
NxDon <nxduanyu@gmail.com>
2017-07-27 22:32:51 +0800

    finish map

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,17 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+    var alphabet = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    return collection.map((elem) => {
+        let index;
+        if (elem % 26 === 0) {
+            index = elem / 26 - 1;
+            return alphabet[index] + alphabet[26];
+        }else{
+            index = Math.floor(elem / 26);
+            return alphabet[index] + alphabet[elem % 26];
+        }
+    });
 };
 
 module.exports = number_map_to_word_over_26;
