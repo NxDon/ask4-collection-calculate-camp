afcff8f657862ffce406c21a6d16c869730a2e7b
NxDon <nxduanyu@gmail.com>
2017-07-27 22:21:41 +0800

    finish flaten

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+    //只适用于数组元素没有"，"的情况
+    var strCollection = collection.join(",").split(",");//数组中是字符串
+    return strCollection.map((elem) => {
+        return parseFloat(elem);
+    })
 }
 
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+    var strCollection = collection.join(",").split(",");//数组中是字符串
+    return strCollection.map((elem) => {
+        return parseFloat(elem);
+    }).filter((elem,index,array) => {
+        return array.indexOf(elem) === index;
+    })
 }
 
 module.exports = double_to_one;
