cea3564430585dc806665ee5c16a7291c81a63c6
NxDon <nxduanyu@gmail.com>
2017-07-28 17:02:37 +0800

    finish section_2

--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -1,5 +1,35 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+
+    var objArr = [];
+    var reg = /\[|\]|-|:/g;
+    collection.map(function (elem) {
+        for (var i = 0; i < objArr.length; i++) {
+            if (elem.replace(reg, ",").split(",")[0] == objArr[i].key) {
+                objArr[i].count += elem.replace(reg, ",").split(",")[1] ? parseInt(elem.replace(reg, ",").split(",")[1]) : 1;
+                return;
+            }
+        }
+        //not in current objArr
+        objArr[objArr.length] = {
+            key: elem.replace(reg, ",").split(",")[0],
+            count: elem.replace(reg, ",").split(",")[1] ? parseInt(elem.replace(reg, ",").split(",")[1]) : 1
+        }
+    })
+    return objArr;
 }
 
+
+var collection = [
+    "a", "a", "a",
+    "e", "e", "e", "e", "e", "e", "e",
+    "h", "h", "h", "h", "h", "h", "h", "h", "h", "h", "h",
+    "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t",
+    "f", "f", "f", "f", "f", "f", "f", "f", "f",
+    "c", "c", "c", "c", "c", "c", "c", "c",
+    "g", "g", "g", "g", "g", "g", "g",
+    "b", "b", "b", "b", "b", "b",
+    "d-5"
+];
+var result = count_same_elements(collection);
+console.log(result);
 module.exports = count_same_elements;
--- practices/section_2/practice_3/practice.js
+++ practices/section_2/practice_3/practice.js
@@ -1,5 +1,21 @@
 function count_same_elements(collection) {
   //在这里写入代码
+    var objArr = [];
+    var reg = /\[|\]|-|:/g;
+    collection.map(function (elem) {
+        for (var i = 0; i < objArr.length; i++) {
+            if (elem.replace(reg, ",").split(",")[0] == objArr[i].name) {
+                objArr[i].summary += elem.replace(reg, ",").split(",")[1] ? parseInt(elem.replace(reg, ",").split(",")[1]) : 1;
+                return;
+            }
+        }
+        //not in current objArr
+        objArr[objArr.length] = {
+            name: elem.replace(reg, ",").split(",")[0],
+            summary: elem.replace(reg, ",").split(",")[1] ? parseInt(elem.replace(reg, ",").split(",")[1]) : 1
+        }
+    })
+    return objArr;
 }
 
 module.exports = count_same_elements;
